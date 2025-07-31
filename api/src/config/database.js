c1e07091b21fc34c668116bc6181666cd9d96093
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-07-31 10:19:34 +0545

    database connection

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,4 +1,14 @@
 import mongoose from "mongoose";
 import config from "./config.js";
 
-const connectDB = async () => {};
+const connectDB = async () => {
+    try {
+        const status = await mongoose.connect(config.mongoDBUrl);
+        console.log(`Database Connected at port ${status.connection.port}`)
+    } catch (error) {
+        console.log(error)
+        process.exit(1);
+    }
+};
+
+export default connectDB;
