f389f80499fc798687438531963f79d1d6857d5a
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-08-01 08:49:25 +0545

    ref's removed, role added and format maintained

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,16 +4,17 @@ const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
+  role: {
+    type: String,
+    default: "USER",
+    enum: ["USER", "ADMIN"],
+  },
   profileImage: String,
   bio: String,
-
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
-  createdRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 
   createdAt: { type: Date, default: Date.now },
 });
 
 const userModel = mongoose.model("User", userSchema);
+
 export default userModel;
