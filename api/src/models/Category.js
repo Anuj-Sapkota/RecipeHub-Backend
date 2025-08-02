b45989f1d7c66bd5f7c8711e8c29f366908cd3f2
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-08-02 20:40:37 +0545

    Category model added

---
+++ api/src/models/Category.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true, trim: true },
+  imageUrl: { type: String, default: "" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
