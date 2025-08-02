8bcf00db1426e543d9a8ef9dc4e802ecbaf1e786
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-08-02 20:44:52 +0545

    comment model added

---
+++ api/src/models/Comment.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true, trim: true },
+  recipe: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Recipe",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const commentModel = mongoose.model("Comment", commentSchema);
+
+export default commentModel;
