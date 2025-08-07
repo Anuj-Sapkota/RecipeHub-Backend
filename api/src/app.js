c51af5845238f6b558802faeed3bce0450cc16d6
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-08-07 17:53:57 +0545

    Body parser and logger added

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import connectDB from "./config/database.js";
 import config from "./config/config.js";
+import bodyParser from "body-parser";
 const app = express();
 
 connectDB();
+
+app.use(bodyParser.json());
+app.use(logger);
 
 app.listen(config.port, () => {
   console.log(`Server running at port: ${config.port}....`);
