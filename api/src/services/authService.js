370109a1000214a5bc552ea03df61d0f1dfe4fdf
Anuj-Sapkota <anujsapkota808@gmail.com>
2025-08-10 11:57:08 +0545

    spaces added where necessary

--- api/src/services/authService.js
+++ api/src/services/authService.js
@@ -4,7 +4,9 @@ import bcrypt from "bcryptjs";
 
 const register = async (data) => {
   const user = await userModel.findOne({ email: data.email });
+
   if (user) throw { message: "User already exists!" };
+
   const hashedPassword = bcrypt.hashSync(data.password); // Password hashing
   const userData = await userModel.create({
     ...data,
@@ -12,25 +14,31 @@ const register = async (data) => {
   });
   const userObj = userData.toObject();
   delete userObj.password;
+
   return userObj;
 };
 
 const login = async (data) => {
   const userData = await userModel.findOne({ email: data.email });
+
   if (!data.email || !data.password)
     throw {
       statusCode: 400,
       message: "Please fill both email and password.",
     };
+    
   const isPasswordMatch = bcrypt.compareSync(data.password, userData.password); // Comparing hashed Password
+
   if (!userData || !isPasswordMatch) {
     throw {
       statusCode: 401,
       message: "Incorrect email or password!",
     };
   }
+
   const userObj = userData.toObject();
   delete userObj.password;
+
   return userObj;
 };
 
