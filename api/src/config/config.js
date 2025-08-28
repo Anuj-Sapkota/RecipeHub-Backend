b090c1e196676c5cc38711ccdfbe942b8869f3e2
Nitin Kumar Shah <shahnitin500@gmail.com>
2025-08-28 19:04:09 +0545

    Fix: casing in MongoDB URL in config file

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  mongoDBUrl: process.env.MONGODB_URL="mongodb+srv://sapkotaanuj10011:itjs79v438IZkQda@mern.l2fa2xl.mongodb.net/recipehub?retryWrites=true&w=majority&appName=Mern"
+  mongoDBUrl: process.env.MONGODB_URL="mongodb+srv://sapkotaanuj10011:itjs79v438IZkQda@mern.l2fa2xl.mongodb.net/RecipeHub?retryWrites=true&w=majority&appName=Mern"
  || "",
   port: process.env.PORT || "5000",
   dbName: process.env.DB_NAME,
